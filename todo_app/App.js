db291cad06dc2d7778ccb699a1ea390214b32f66
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-12 19:53:21 -0300

    feat: created layout of tasks list

--- todo_app/App.js
+++ todo_app/App.js
@@ -1,16 +1,48 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+} from 'react-native';
+
 import Login from './src/components/Login';
+import TaskList from './src/components/TaskList';
+
+let tasks = [
+  {key: '1', nome: 'Comprar Coca cola'},
+  {key: '2', nome: 'Estudar javascript'},
+];
 
 export default function App() {
   const [user, setUser] = useState(null);
+
+  const [newTask, setNewTask] = useState('');
 
   if (!user) {
     return <Login changeStatus={user => setUser(user)} />;
   }
   return (
     <SafeAreaView style={styles.container}>
-      <Text>DENTRO DA TELA DE TAREFAS</Text>
+      <View style={styles.containerTask}>
+        <TextInput
+          style={styles.input}
+          placeholder="O que vai fazer hoje?"
+          value={newTask}
+          onChangeText={text => setNewTask(text)}
+        />
+        <TouchableOpacity style={styles.buttonAdd}>
+          <Text>+</Text>
+        </TouchableOpacity>
+      </View>
+      <FlatList
+        data={tasks}
+        keyExtractor={item => item.key}
+        renderItem={({item}) => <TaskList data={item} />}
+      />
     </SafeAreaView>
   );
 }
@@ -21,5 +53,31 @@ const styles = StyleSheet.create({
     paddingTop: 25,
     paddingHorizontal: 10,
     backgroundColor: '#F2F6FC',
+  },
+  containerTask: {
+    flexDirection: 'row',
+  },
+  input: {
+    flex: 1,
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: '#FFF',
+    borderRadius: 4,
+    borderWidth: 1,
+    borderColor: '#141414',
+    height: 45,
+  },
+  buttonAdd: {
+    backgroundColor: '#141414',
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 5,
+    paddingHorizontal: 14,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: '#FFF',
+    fontSize: 22,
   },
 });
---
+++ todo_app/src/components/TaskList/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+export default function TaskList({data}) {
+  return (
+    <View>
+      <Text>
+        {' '}
+        {data.key} {data.nome}{' '}
+      </Text>
+    </View>
+  );
+}
