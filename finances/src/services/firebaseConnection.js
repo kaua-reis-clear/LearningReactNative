3719ee0716c529c5288f175ca0952d10c62ed6c5
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-18 10:31:41 -0300

    chore: installed missing dependencies and removed wrong packages

--- finances/src/services/firebaseConnection.js
+++ finances/src/services/firebaseConnection.js
@@ -1,25 +1,20 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/database';
+// Import the functions you need from the SDKs you need
+import {initializeApp} from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyADY2DGXNYbqOu1KKX6VHhl34j4BysxYic',
-
   authDomain: 'finances-fdce1.firebaseapp.com',
-
+  databaseURL: 'https://finances-fdce1-default-rtdb.firebaseio.com',
   projectId: 'finances-fdce1',
-
   storageBucket: 'finances-fdce1.appspot.com',
-
   messagingSenderId: '411215053268',
-
   appId: '1:411215053268:web:2659bc9ca89b92361a1641',
-
   measurementId: 'G-NSKMCRQ0TK',
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export default firebase;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
