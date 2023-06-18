de6f509f29e35e117a0e380a5a184237d662b657
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-18 19:56:14 -0300

    feat: calculating the amount in cart

--- shopping_cart/src/pages/Cart/index.js
+++ shopping_cart/src/pages/Cart/index.js
@@ -6,7 +6,9 @@ import { CartContext } from '../../contexts/CartContext';
 import CardItem from '../../components/CardItem';
 
 export default function Cart() {
-  const { cart, addItemCart, removeItemCart } = useContext(CartContext);
+  const {
+    cart, addItemCart, removeItemCart, total,
+  } = useContext(CartContext);
 
   return (
     <View style={stlyes.container}>
@@ -22,6 +24,13 @@ export default function Cart() {
             removeAmount={() => removeItemCart(item)}
           />
         )}
+        ListFooterComponent={() => (
+          <Text style={stlyes.total}>
+            Total: R$
+            {' '}
+            {total}
+          </Text>
+        )}
       />
     </View>
   );
@@ -34,5 +43,10 @@ const stlyes = StyleSheet.create({
     paddingStart: 14,
     paddingEnd: 14,
     paddingTop: 14,
+  },
+  total: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 24,
   },
 });
