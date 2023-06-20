709bc9dbd5ddf1ba0413057debe5676ca94c853b
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-19 23:03:34 -0300

    feat: created camera project

---
+++ camera/App.js
@@ -0,0 +1,11 @@
+import {View, Text, StatusBar} from 'react-native';
+import React from 'react';
+
+export default function App() {
+  return (
+    <View>
+      <StatusBar hidden={true} />
+      <Text>Olá Mundo!</Text>
+    </View>
+  );
+}
