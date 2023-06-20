044b79df083a8c40c279e3842e1a05c083a6ea14
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-19 23:05:45 -0300

    feat: configured camera with react native

--- camera/App.js
+++ camera/App.js
@@ -1,11 +1,95 @@
-import {View, Text, StatusBar} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
 export default function App() {
+  const [type, setType] = useState(RNCamera.Constants.Type.back);
+  const [open, setOpen] = useState(false);
+
+  function takePicture() {
+    setOpen(true);
+  }
+
   return (
-    <View>
+    <View style={styles.container}>
       <StatusBar hidden={true} />
-      <Text>Olá Mundo!</Text>
+
+      <RNCamera
+        style={styles.preview}
+        type={type}
+        flashMode={RNCamera.Constants.FlashMode.auto}
+        androidCameraPermissionOptions={{
+          title: 'Permissao para usar a camera',
+          message: 'Nós precisamos usar a sua camera',
+          buttonPositive: 'Ok',
+          buttonNegative: 'Cancelar',
+        }}>
+        {({camera, status, recordAndroidPermissionStatus}) => {
+          if (status !== 'READY') {
+            return <View />;
+          }
+          return (
+            <View
+              style={{
+                marginBottom: 35,
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+                justifyContent: 'space-between',
+              }}>
+              <TouchableOpacity
+                onPress={() => takePicture()}
+                style={styles.capture}>
+                <Text>Tirar foto</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {}} style={styles.capture}>
+                <Text>Album</Text>
+              </TouchableOpacity>
+            </View>
+          );
+        }}
+      </RNCamera>
+
+      <Modal animationType="slide" transparent={false} visible={open}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            margin: 20,
+          }}>
+          <TouchableOpacity style={{margin: 10}} onPress={() => setOpen(false)}>
+            <Text style={{fontSize: 24}}>Fechar</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
