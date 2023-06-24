74c5d54428a2d4982565e964c5aca03dc259b928
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-24 17:59:56 -0300

    feat: finished 'mega-sena' component

--- exercices/src/components/mega/Mega.js
+++ exercices/src/components/mega/Mega.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
-import {Button, Text, TextInput} from 'react-native';
+import {View, Text, TextInput, Button} from 'react-native';
 import Estilo from '../estilo';
+
+import MegaNumero from './MegaNumero';
 
 export default class Mega extends Component {
   state = {
@@ -25,6 +27,13 @@ export default class Mega extends Component {
     this.setState({numeros});
   };
 
+  exibirNumeros = () => {
+    const nums = this.state.numeros;
+    return nums.map(num => {
+      return <MegaNumero key={num} num={num} />;
+    });
+  };
+
   render() {
     return (
       <>
@@ -37,7 +46,15 @@ export default class Mega extends Component {
           onChangeText={this.alterarQtdeNumero}
         />
         <Button title="Gerar" onPress={this.gerarNumeros} />
-        <Text>{this.state.numeros.join(',')}</Text>
+        <View
+          style={{
+            marginTop: 20,
+            flexDirection: 'row',
+            flexWrap: 'wrap',
+            justifyContent: 'center',
+          }}>
+          {this.exibirNumeros()}
+        </View>
       </>
     );
   }
