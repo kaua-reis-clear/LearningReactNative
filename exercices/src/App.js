220a7f9fdec3194a2fbede8017acb6319a49d109
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-24 17:23:47 -0300

    feat: created controlled component

--- exercices/src/App.js
+++ exercices/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet} from 'react-native';
 
-import ListaProdutosV2 from './components/produtos/ListaProdutosV2';
+import DigiteSeuNome from './components/DigiteSeuNome';
+// import ListaProdutosV2 from './components/produtos/ListaProdutosV2'
 // import ListaProdutos from './components/produtos/ListaProdutos'
 // import UsuarioLogado from './components/UsuarioLogado'
 // import Familia from './components/relacao/Familia'
@@ -21,8 +22,9 @@ import ListaProdutosV2 from './components/produtos/ListaProdutosV2';
 
 export default () => (
   <SafeAreaView style={style.App}>
-    <ListaProdutosV2 />
+    <DigiteSeuNome />
     {/*
+        <ListaProdutosV2 />
         <ListaProdutos />
         <UsuarioLogado usuario={{ nome:"Gui", email:"gui@gui.com" }} />
         <UsuarioLogado usuario={{ nome:"Ana" }} />
