3b446a299093c80f7efe4cecbd2d991039a305ac
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-24 23:34:40 -0300

    feat: created mine component

--- mines/App.js
+++ mines/App.js
@@ -1,18 +1,26 @@
 import {Text, StyleSheet, View} from 'react-native';
 import React, {Component} from 'react';
 import Field from './src/components/Field';
+import params from './src/params';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Iniciando o mines</Text>
+        <Text>Iniciando o Mines!!!</Text>
+        <Text>
+          Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount}
+        </Text>
         <Field />
         <Field opened />
         <Field opened nearMines={1} />
         <Field opened nearMines={2} />
         <Field opened nearMines={3} />
         <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
       </View>
     );
   }
@@ -24,5 +32,10 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
   },
 });
--- mines/src/components/Field.js
+++ mines/src/components/Field.js
@@ -1,13 +1,17 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import params from '../params';
+import Mine from './Mine';
 
 export default props => {
-  const {mined, opened, nearMines} = props;
+  const {mined, opened, nearMines, exploded} = props;
 
   const styleField = [styles.field];
   if (opened) {
     styleField.push(styles.opened);
+  }
+  if (exploded) {
+    styleField.push(styles.exploded);
   }
   if (styleField.length === 1) {
     styleField.push(styles.regular);
@@ -36,6 +40,7 @@ export default props => {
       ) : (
         false
       )}
+      {mined && opened ? <Mine /> : false}
     </View>
   );
 };
@@ -62,5 +67,9 @@ const styles = StyleSheet.create({
   label: {
     fontWeight: 'bold',
     fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red',
   },
 });
