ae7cbd4c1d6a2a0f86589c7b832982423564c4c9
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-25 14:53:25 -0300

    feat: created user form

--- rncrud/src/views/UserForm.js
+++ rncrud/src/views/UserForm.js
@@ -1,6 +1,50 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Button, TextInput} from 'react-native';
+import {Text, View, StyleSheet} from 'react-native';
 
-export default props => {
-  return <Text>UserListForm</Text>;
+export default ({route, navigation}) => {
+  const [user, setUser] = useState(route.params ? route.params : {});
+  return (
+    <View style={styles.form}>
+      <Text>Nome</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={name => setUser({...user, name})}
+        placeholder="Informe o Nome"
+        value={user.name}
+      />
+      <Text>Email</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={email => setUser({...user, email})}
+        placeholder="Informe o E-mail"
+        value={user.email}
+      />
+      <Text>URL do Avatar</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={avatarUrl => setUser({...user, avatarUrl})}
+        placeholder="Informe a URL do Avatar"
+        value={user.avatarUrl}
+      />
+      <Button
+        title="Salvar"
+        onPress={() => {
+          navigation.goBack();
+        }}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 12,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+  },
+});
