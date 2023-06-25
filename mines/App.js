a1dce1b03fc4805e1277d204fa74737d2f2b02aa
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-25 01:49:08 -0300

    feat: created mine field component

--- mines/App.js
+++ mines/App.js
@@ -3,26 +3,37 @@ import React, {Component} from 'react';
 import Field from './src/components/Field';
 import params from './src/params';
 import Flag from './src/components/Flag';
+import MineField from './src/components/MineField';
+import {createMineBoard} from './src/functions';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = this.createState();
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return Math.ceil(cols * rows * params.difficultLevel);
+  };
+
+  createState = () => {
+    const cols = params.getColumnsAmount();
+    const rows = params.getRowsAmount();
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount()),
+    };
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Iniciando o Mines!!!</Text>
-        <Text>
+        <Text style={styles.welcome}>Iniciando o Mines!!!</Text>
+        <Text style={styles.instructions}>
           Tamanho da grade:
           {params.getRowsAmount()}x{params.getColumnsAmount}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged opened />
       </View>
     );
   }
@@ -32,16 +43,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333',
+    backgroundColor: '#AAA',
   },
 });
