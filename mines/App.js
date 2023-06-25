083d1c7e2d06d7a1fbd5701806e85636d2c0c8cc
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-24 23:02:47 -0300

    feat: started mines project

---
+++ mines/App.js
@@ -0,0 +1,21 @@
+import {Text, StyleSheet, View} from 'react-native';
+import React, {Component} from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+});
