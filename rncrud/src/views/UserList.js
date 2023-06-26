f2b5042acb606aa01f8f7e02ab4736910273c090
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-26 08:14:01 -0300

    feat: created reducer in users context

--- rncrud/src/views/UserList.js
+++ rncrud/src/views/UserList.js
@@ -4,13 +4,17 @@ import {Button, Icon, ListItem} from 'react-native-elements';
 import UsersContext from '../context/UserContext';
 
 export default props => {
-  const {state} = useContext(UsersContext);
+  const {state, dispatch} = useContext(UsersContext);
+
   function confirmUserDeletion(user) {
     Alert.alert('Excluir Usuário', 'Deseja excluir o usuário', [
       {
         text: 'Sim',
         onPress() {
-          console.warn('delete ' + user.id);
+          dispatch({
+            type: 'deleteUser',
+            payload: user,
+          });
         },
       },
       {
