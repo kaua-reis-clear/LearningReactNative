5289f2ab0193a7d4eb6ca46c8c44163f9e6f4be5
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-26 22:20:43 -0300

    feat: created filter task function

--- tasks_offline/src/screens/TaskList.js
+++ tasks_offline/src/screens/TaskList.js
@@ -1,8 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text, ImageBackground, StyleSheet, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  StyleSheet,
+  FlatList,
+  TouchableOpacity,
+  Platform,
+} from 'react-native';
 
 import commonStyles from '../commonStyles';
 import todayImage from '../../assets/imgs/today.jpg';
+
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 import moment from 'moment';
 import 'moment/locale/pt-br';
@@ -11,6 +21,7 @@ import Task from '../components/Task';
 
 export default class TaskList extends Component {
   state = {
+    showDoneTasks: true,
     tasks: [
       {
         id: Math.random(),
@@ -25,6 +36,10 @@ export default class TaskList extends Component {
         doneAt: null,
       },
     ],
+  };
+
+  toggleFilter = () => {
+    this.setState({showDoneTasks: !this.state.showDoneTasks});
   };
 
   toggleTask = taskId => {
@@ -45,6 +60,15 @@ export default class TaskList extends Component {
     return (
       <View style={styles.container}>
         <ImageBackground source={todayImage} style={styles.background}>
+          <View style={styles.iconBar}>
+            <TouchableOpacity onPress={this.toggleFilter}>
+              <Icon
+                name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                size={20}
+                color={commonStyles.colors.secondary}
+              />
+            </TouchableOpacity>
+          </View>
           <View style={styles.titleBar}>
             <Text style={styles.title}>Hoje</Text>
             <Text style={styles.subtitle}>{today}</Text>
@@ -91,5 +115,11 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginLeft: 20,
     marginBottom: 30,
+  },
+  iconBar: {
+    flexDirection: 'row',
+    marginHorizontal: 20,
+    justifyContent: 'flex-end',
+    marginTop: Platform.OS === 'ios' ? 40 : 10,
   },
 });
