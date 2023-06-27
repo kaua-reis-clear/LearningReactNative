f371101f22427f151461cc1b37732a5afe8a6a18
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-26 22:27:47 -0300

    feat: created add task modal

--- tasks_offline/src/screens/TaskList.js
+++ tasks_offline/src/screens/TaskList.js
@@ -18,10 +18,13 @@ import moment from 'moment';
 import 'moment/locale/pt-br';
 
 import Task from '../components/Task';
+import AddTask from './AddTask';
 
 export default class TaskList extends Component {
   state = {
     showDoneTasks: true,
+    showAddTask: false,
+    visibleTasks: [],
     tasks: [
       {
         id: Math.random(),
@@ -38,8 +41,24 @@ export default class TaskList extends Component {
     ],
   };
 
+  componentDidMount = () => {
+    this.filterTasks();
+  };
+
   toggleFilter = () => {
-    this.setState({showDoneTasks: !this.state.showDoneTasks});
+    this.setState({showDoneTasks: !this.state.showDoneTasks}, this.filterTasks);
+  };
+
+  filterTasks = () => {
+    let visibleTasks = null;
+    if (this.state.showDoneTasks) {
+      visibleTasks = [...this.state.tasks];
+    } else {
+      const pending = task => task.doneAt === null;
+      visibleTasks = this.state.tasks.filter(pending);
+    }
+
+    this.setState({visibleTasks});
   };
 
   toggleTask = taskId => {
@@ -50,7 +69,7 @@ export default class TaskList extends Component {
       }
     });
 
-    this.setState({tasks});
+    this.setState({tasks}, this.filterTasks);
   };
 
   render() {
@@ -59,6 +78,10 @@ export default class TaskList extends Component {
       .format('ddd, D [de] MMMM');
     return (
       <View style={styles.container}>
+        <AddTask
+          isVisible={this.state.showAddTask}
+          onCancel={() => this.setState({showAddTask: false})}
+        />
         <ImageBackground source={todayImage} style={styles.background}>
           <View style={styles.iconBar}>
             <TouchableOpacity onPress={this.toggleFilter}>
@@ -76,13 +99,19 @@ export default class TaskList extends Component {
         </ImageBackground>
         <View style={styles.taskList}>
           <FlatList
-            data={this.state.tasks}
+            data={this.state.visibleTasks}
             keyExtractor={item => `${item.id}`}
             renderItem={({item}) => (
               <Task {...item} toggleTask={this.toggleTask} />
             )}
           />
         </View>
+        <TouchableOpacity
+          style={styles.addButton}
+          activeOpacity={0.7}
+          onPress={() => this.setState({showAddTask: true})}>
+          <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -121,5 +150,16 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     justifyContent: 'flex-end',
     marginTop: Platform.OS === 'ios' ? 40 : 10,
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
