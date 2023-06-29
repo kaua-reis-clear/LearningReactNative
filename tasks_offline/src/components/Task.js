190e6e72d320d46dde87a915b891db79f06e9d13
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-06-28 22:23:46 -0300

    feat: created method to delete task

--- tasks_offline/src/components/Task.js
+++ tasks_offline/src/components/Task.js
@@ -31,8 +31,19 @@ export default props => {
     );
   };
 
+  const getLeftContent = () => {
+    return (
+      <View style={styles.left}>
+        <Icon name="trash" size={20} color="#FFF" style={styles.excludeIcon} />
+        <Text style={styles.excludeText}>Excluir</Text>
+      </View>
+    );
+  };
+
   return (
-    <Swipeable renderRightActions={getRightContent}>
+    <Swipeable
+      renderRightActions={getRightContent}
+      renderLeftActions={getLeftContent}>
       <View style={styles.container}>
         <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
           <View style={styles.checkContainer}>
@@ -67,6 +78,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     alignItems: 'center',
     paddingVertical: 10,
+    backgroundColor: '#FFF',
   },
   checkContainer: {
     width: '20%',
@@ -104,5 +116,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'flex-end',
     paddingHorizontal: 20,
+  },
+  left: {
+    flex: 1,
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  excludeIcon: {
+    marginLeft: 10,
+  },
+  excludeText: {
+    fontFamily: commonStyles.fontFamily,
+    color: '#FFF',
+    fontSize: 20,
+    margin: 10,
   },
 });
