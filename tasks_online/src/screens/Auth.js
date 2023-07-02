988be0274fab03536f8b3fa944a646e988703aa0
KauaDrar <kauadrar@gmail.com>
2023-07-02 19:10:23 -0300

    feat: created logics to allow user sign in or not

--- tasks_online/src/screens/Auth.js
+++ tasks_online/src/screens/Auth.js
@@ -9,6 +9,7 @@ import {
 } from 'react-native';
 
 import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import backgroundImage from '../../assets/imgs/login.jpg';
 import commonStyles from '../commonStyles';
@@ -18,8 +19,8 @@ import {server, showError, showSuccess} from '../common';
 
 const initialState = {
   name: '',
-  email: 'kauadrar@gmail.com.br',
-  password: 'password',
+  email: '',
+  password: '',
   confirmPassword: '',
   stageNew: false,
 };
@@ -60,6 +61,7 @@ export default class Auth extends Component {
         password: this.state.password,
       });
 
+      AsyncStorage.setItem('userData', JSON.stringify(res.data));
       axios.defaults.headers.common.Authorization = `bearer ${res.data.token}`;
       this.props.navigation.navigate('Home', res.data);
     } catch (e) {
