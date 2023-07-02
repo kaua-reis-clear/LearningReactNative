73ac740582d4903001a4138783e1b79083553b76
KauaDrar <kauadrar@gmail.com>
2023-07-02 19:12:15 -0300

    feat: created logout button and method

--- tasks_online/src/screens/Menu.js
+++ tasks_online/src/screens/Menu.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import {Platform, ScrollView, View, Text, StyleSheet} from 'react-native';
+import {
+  Platform,
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
 import {DrawerItems} from 'react-navigation-drawer';
 import {Gravatar} from 'react-native-gravatar';
 import commonStyles from '../commonStyles';
 
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
 export default props => {
+  const logout = () => {
+    delete axios.defaults.headers.common.Authorization;
+    AsyncStorage.removeItem('userData');
+    props.navigation.navigate('AuthOrApp');
+  };
+
   return (
     <ScrollView>
       <View style={styles.header}>
@@ -20,6 +37,11 @@ export default props => {
           <Text style={styles.name}>{props.navigation.getParam('name')}</Text>
           <Text style={styles.email}>{props.navigation.getParam('email')}</Text>
         </View>
+        <TouchableOpacity onPress={logout}>
+          <View style={styles.logoutIcon}>
+            <Icon name="sign-out" size={30} color="#800" />
+          </View>
+        </TouchableOpacity>
       </View>
       <DrawerItems {...props} />
     </ScrollView>
@@ -59,6 +81,10 @@ const styles = StyleSheet.create({
     fontFamily: commonStyles.fontFamily,
     fontSize: 15,
     color: commonStyles.colors.subText,
+    marginBottom: 10,
+  },
+  logoutIcon: {
+    marginLeft: 10,
     marginBottom: 10,
   },
 });
