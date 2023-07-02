46f34156e8d42527274a78b89923485f2a477713
KauaDrar <kauadrar@gmail.com>
2023-07-02 17:33:24 -0300

    feat: created tasks online project backend

---
+++ tasks_backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+const consign = require('consign');
+const db = require('./config/db');
+
+consign()
+  .include('./config/passport.js')
+  .then('./config/middlewares.js')
+  .then('./api')
+  .then('./config/routes.js')
+  .into(app);
+
+app.db = db;
+
+app.listen(3000, () => {
+  console.log('Backend executando...');
+});
