b2b1cc2b6c70feb333ecccec783a6ea6e8b5781e
KauaDrar <kauadrar@gmail.com>
2023-07-03 22:34:12 -0300

    feat: created post component

--- lambe/App.js
+++ lambe/App.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import {View} from 'react-native';
 import Header from './src/components/Header';
+import Post from './src/components/Post';
 
-export default props => <Header />;
+export default props => (
+  <View style={{flex: 1}}>
+    <Header />
+    <Post image={require('./assets/imgs/fence.jpg')} />
+  </View>
+);
