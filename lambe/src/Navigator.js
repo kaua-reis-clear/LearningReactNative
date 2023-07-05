cbc5e91647b01efd27440e1dccdcf3c8f95587c4
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-04 22:57:59 -0300

    feat: crated tab navigator

---
+++ lambe/src/Navigator.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+
+import Feed from './screens/Feed';
+
+const Tab = createBottomTabNavigator();
+
+const routeIcon = {
+  Feed: 'home',
+  AddPhoto: 'camera',
+  Profile: 'person',
+};
+
+export default props => (
+  <NavigationContainer>
+    <Tab.Navigator
+      initialRouteName="Feed"
+      screenOptions={({route}) => ({
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarIcon: ({color, size}) => (
+          <MaterialIcons
+            name={routeIcon[route.name]}
+            size={size}
+            color={color}
+          />
+        ),
+      })}>
+      <Tab.Screen name="Feed" component={Feed} />
+      <Tab.Screen name="AddPhoto" component={Feed} />
+      <Tab.Screen name="Profile" component={Feed} />
+    </Tab.Navigator>
+  </NavigationContainer>
+);
