78f5a024285bc17a52536c752b2caa65e8b75823
kaua-reis-clear <kaua.reis.clear@gmail.com>
2023-07-06 23:57:15 -0300

    feat: created logic to conditional render a screen

--- lambe/index.js
+++ lambe/index.js
@@ -2,6 +2,7 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import {AppRegistry} from 'react-native';
 import Navigator from './src/Navigator';
 import {name as appName} from './app.json';
--- lambe/src/Navigator.js
+++ lambe/src/Navigator.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import Feed from './screens/Feed';
@@ -9,6 +10,7 @@ import Profile from './screens/Profile';
 import Login from './screens/Login';
 
 const Tab = createBottomTabNavigator();
+const SwitchStack = createStackNavigator();
 
 const routeIcon = {
   Feed: 'home',
@@ -16,26 +18,34 @@ const routeIcon = {
   Profile: 'person',
 };
 
-export default props => (
-  <NavigationContainer>
-    <Tab.Navigator
-      initialRouteName="Feed"
-      screenOptions={({route}) => ({
-        headerShown: false,
-        tabBarShowLabel: false,
-        tabBarIcon: ({color, size}) => (
-          <MaterialIcons
-            name={routeIcon[route.name]}
-            size={size}
-            color={color}
-          />
-        ),
-      })}>
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="AddPhoto" component={AddPhoto} />
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Profile" component={Profile} />
-      <Tab.Screen name="Login" component={Login} />
-    </Tab.Navigator>
-  </NavigationContainer>
-);
+export default props => {
+  const [isLogged, setIsLogged] = useState(false);
+
+  const AuthOrProfile = () => (
+    <SwitchStack.Navigator screenOptions={{headerShown: false}}>
+      {isLogged ? (
+        <SwitchStack.Screen name="Home" component={Profile} />
+      ) : (
+        <SwitchStack.Screen name="Auth" component={Login} />
+      )}
+    </SwitchStack.Navigator>
+  );
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={({route}) => ({
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarIcon: ({color, size}) => (
+            <Ionicons name={routeIcon[route.name]} size={size} color={color} />
+          ),
+        })}>
+        <Tab.Screen name="Feed" component={Feed} />
+        <Tab.Screen name="AddPhoto" component={AddPhoto} />
+        <Tab.Screen name="Profile" component={AuthOrProfile} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
